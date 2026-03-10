e7c93c5fe1e038083da4b70119f7f50ca01f9e6b
Eric <eric@webtropolis.de>
2026-03-10 13:18:42 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/web-habit-tracker/sw.js', { scope: '/web-habit-tracker/' })})}
