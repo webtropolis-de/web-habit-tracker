6229967f75feb2d85e04cff9a4d1982eb219582c
Eric <eric@webtropolis.de>
2026-03-10 15:17:34 +0100

    security fix

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://jbccrsbjcdtnwihloqsd.supabase.co";
-const supabaseAnonKey = "sb_publishable_OlMNJJA8NSNitsQhDdKM_Q_Z7QZyu2c";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
