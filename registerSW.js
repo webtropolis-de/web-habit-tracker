b5f28341874004d6c1a4a0246066988758646bb4
webtropoliseric <eric.hermann.berlin@googlemail.com>
2026-03-17 20:42:50 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/web-habit-tracker/sw.js', { scope: '/web-habit-tracker/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
